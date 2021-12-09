25ff15e52a87cf886992dc39b34380fca98478b5
Simik31 <simik31@pm.me>
2021-12-09 21:05:09 +0100

    Day 2 | Node.js

---
+++ nodejs/Day2.js
@@ -0,0 +1,54 @@
+require('./utils.js');
+
+module.exports = {
+    this:part1 = function () {
+        var h_pos = 0, v_pos = 0;
+
+        readFile('../data/day2.txt').forEach((line) => {
+            var value = Number(line.split(' ')[1]);
+
+            switch (line.split(' ')[0])
+            {
+                case "forward":
+                    h_pos += value;
+                    break;
+                case "down":
+                    v_pos += value;
+                    break;
+                case "up":
+                    v_pos -= value;
+                    break;
+                default:
+                    console.error("Unsupported direction " + line.split(' ')[0]);
+            }
+        })
+
+        console.info("Day:  2 | Part: 1 | Result: " + (h_pos * v_pos))
+    },
+
+    this:part2 = function () {
+        var h_pos = 0, v_pos = 0, aim = 0;
+
+        readFile('../data/day2.txt').forEach((line) => {
+            var value = Number(line.split(' ')[1]);
+
+            switch (line.split(' ')[0])
+            {
+                case "forward":
+                    h_pos += value;
+                    v_pos += value * aim;
+                    break;
+                case "down":
+                    aim += value;
+                    break;
+                case "up":
+                    aim -= value;
+                    break;
+                default:
+                    console.error("Unsupported direction " + line.split(' ')[0]);
+            }
+        })
+
+        console.info("Day:  2 | Part: 1 | Result: " + (h_pos * v_pos))
+    }
+}
