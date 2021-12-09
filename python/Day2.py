c33bd63e7adca553b35c7dd781c7792680d0984a
Simik31 <simik31@pm.me>
2021-12-09 21:09:01 +0100

    Day 2 | Python

---
+++ python/Day2.py
@@ -0,0 +1,47 @@
+def part_1() -> None:   
+    h_pos: int = 0
+    v_pos: int = 0
+     
+    with open("../data/day2.txt", "r") as dFile:
+        for row in dFile.readlines():     
+            value: int = int(row.split(" ")[1])
+            
+            match row.split(" ")[0]:
+                case "forward":
+                    h_pos += value
+                case "down":
+                    v_pos += value
+                case "up":
+                    v_pos -= value
+                case _:
+                    raise ValueError(f"Unsupported command {row.split(' ')[0]}")
+
+    print("Day:  2 | Part: 1 | Result:", h_pos * v_pos)
+
+
+def part_2() -> None:
+    h_pos: int = 0
+    v_pos: int = 0
+    aim : int = 0
+     
+    with open("../data/day2.txt", "r") as dFile:
+        for row in dFile.readlines():     
+            value: int = int(row.split(" ")[1])
+            
+            match row.split(" ")[0]:
+                case "forward":
+                    h_pos += value
+                    v_pos += value * aim
+                case "down":
+                    aim += value
+                case "up":
+                    aim -= value
+                case _:
+                    raise ValueError(f"Unsupported command {row.split(' ')[0]}")
+
+    print("Day:  2 | Part: 1 | Result:", h_pos * v_pos)
+
+
+if __name__ == "__main__":
+    part_1()
+    part_2()
--- python/main.py
+++ python/main.py
@@ -1,9 +1,12 @@
 import Day1
+import Day2
 
 
 def main() -> None:
     Day1.part_1()
     Day1.part_2()
+    Day2.part_1()
+    Day2.part_2()
 
 
 if __name__ == "__main__":
