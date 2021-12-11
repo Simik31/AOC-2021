8b82bb360482c935aa02e5982168df1385b4be83
Simik31 <simik31@pm.me>
2021-12-11 23:49:45 +0100

    Day 6 | C++ | Edit: using namespace std;

--- cpp/Day6.cpp
+++ cpp/Day6.cpp
@@ -2,19 +2,19 @@
 
 void Day6::part1()
 {
-    std::vector<std::string> lines;
+    vector<string> lines;
     Utils::read_lines_from_file("../data/day6.txt", lines);
        
-    std::map<int, int> ages;
-    std::vector<std::string> num_s_v;
+    map<int, int> ages;
+    vector<string> num_s_v;
     Utils::split_string(lines[0], ",", num_s_v);
 
     for (int age = -1; age < 9; age++)
-        ages.insert(std::pair<int, int>(age, 0));
+        ages.insert(pair<int, int>(age, 0));
 
-    for (std::string num_s : num_s_v)
+    for (string num_s : num_s_v)
     {
-        ages[std::stoi(num_s)]++;
+        ages[stoi(num_s)]++;
     }
 
     for (int day = 0; day < 80; day++)
@@ -29,24 +29,24 @@ void Day6::part1()
     for (int age = 0; age < 9; age++)
         result += ages[age];
 
-    std::cout << "Day:  6 | Part: 1 | Result: " << result << std::endl;
+    cout << "Day:  6 | Part: 1 | Result: " << result << endl;
 }
 
 void Day6::part2()
 {
-    std::vector<std::string> lines;
+    vector<string> lines;
     Utils::read_lines_from_file("../data/day6.txt", lines);
 
-    std::map<int, long long> ages;
-    std::vector<std::string> num_s_v;
+    map<int, long long> ages;
+    vector<string> num_s_v;
     Utils::split_string(lines[0], ",", num_s_v);
 
     for (int age = -1; age < 9; age++)
-        ages.insert(std::pair<int, long long>(age, 0L));
+        ages.insert(pair<int, long long>(age, 0L));
 
-    for (std::string num_s : num_s_v)
+    for (string num_s : num_s_v)
     {
-        ages[std::stoi(num_s)]++;
+        ages[stoi(num_s)]++;
     }
 
     for (int day = 0; day < 256; day++)
@@ -61,5 +61,5 @@ void Day6::part2()
     for (int age = 0; age < 9; age++)
         result += ages[age];
 
-    std::cout << "Day:  6 | Part: 2 | Result: " << result << std::endl;
+    cout << "Day:  6 | Part: 2 | Result: " << result << endl;
 }
