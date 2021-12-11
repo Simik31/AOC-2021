01af6766d9d65f0b63177c5f70865297df09aa37
Simik31 <simik31@pm.me>
2021-12-11 23:49:37 +0100

    Day 5 | C++ | Edit: using namespace std;

--- cpp/Day5.cpp
+++ cpp/Day5.cpp
@@ -2,41 +2,41 @@
 
 void Day5::part1()
 {
-	std::vector<int> x1;
-	std::vector<int> y1;
-	std::vector<int> x2;
-	std::vector<int> y2;
+	vector<int> x1;
+	vector<int> y1;
+	vector<int> x2;
+	vector<int> y2;
 
-	std::vector<std::string> lines;
+	vector<string> lines;
 	Utils::read_lines_from_file("../data/day5.txt", lines);
 
-	std::vector<std::string> points, one, two;
-	for (std::string line : lines)
+	vector<string> points, one, two;
+	for (string line : lines)
 	{
 		Utils::split_string(line, " -> ", points);
 		Utils::split_string(points[0], ",", one);
 		Utils::split_string(points[1], ",", two);
 
-		x1.push_back(std::stoi(one[0]));
-		y1.push_back(std::stoi(one[1]));
-		x2.push_back(std::stoi(two[0]));
-		y2.push_back(std::stoi(two[1]));
+		x1.push_back(stoi(one[0]));
+		y1.push_back(stoi(one[1]));
+		x2.push_back(stoi(two[0]));
+		y2.push_back(stoi(two[1]));
 	}
 
-	int max_x = std::max(
-		*std::max_element(x1.begin(), x1.end()),
-		*std::max_element(x2.begin(), x2.end())
+	int max_x = max(
+		*max_element(x1.begin(), x1.end()),
+		*max_element(x2.begin(), x2.end())
 	);
-	int max_y = std::max(
-		*std::max_element(y1.begin(), y1.end()),
-		*std::max_element(y2.begin(), y2.end())
+	int max_y = max(
+		*max_element(y1.begin(), y1.end()),
+		*max_element(y2.begin(), y2.end())
 	);
 
-	std::vector<std::vector<int>> vents;
+	vector<vector<int>> vents;
 
 	for (int y = 0; y <= max_y; y++)
 	{
-		vents.push_back(std::vector<int>());
+		vents.push_back(vector<int>());
 		for (int x = 0; x <= max_x; x++)
 			vents[y].push_back(0);
 	}
@@ -48,8 +48,8 @@ void Day5::part1()
 		int x_two = x2[index];
 		int y_two = y2[index];
 
-		if (x_one > x_two) std::swap(x_one, x_two);
-		if (y_one > y_two) std::swap(y_one, y_two);
+		if (x_one > x_two) swap(x_one, x_two);
+		if (y_one > y_two) swap(y_one, y_two);
 
 		if (x_one == x_two)
 			for (int y = y_one; y <= y_two; y++)
@@ -66,46 +66,46 @@ void Day5::part1()
 			if (vents[i][cell] >= 2)
 				counter++;
 
-	std::cout << "Day:  5 | Part: 1 | Result: " << counter << std::endl;
+	cout << "Day:  5 | Part: 1 | Result: " << counter << endl;
 }
 
 void Day5::part2()
 {
-	std::vector<int> x1;
-	std::vector<int> y1;
-	std::vector<int> x2;
-	std::vector<int> y2;
+	vector<int> x1;
+	vector<int> y1;
+	vector<int> x2;
+	vector<int> y2;
 
-	std::vector<std::string> lines;
+	vector<string> lines;
 	Utils::read_lines_from_file("../data/day5.txt", lines);
 
-	std::vector<std::string> points, one, two;
-	for (std::string line : lines)
+	vector<string> points, one, two;
+	for (string line : lines)
 	{
 		Utils::split_string(line, " -> ", points);
 		Utils::split_string(points[0], ",", one);
 		Utils::split_string(points[1], ",", two);
 
-		x1.push_back(std::stoi(one[0]));
-		y1.push_back(std::stoi(one[1]));
-		x2.push_back(std::stoi(two[0]));
-		y2.push_back(std::stoi(two[1]));
+		x1.push_back(stoi(one[0]));
+		y1.push_back(stoi(one[1]));
+		x2.push_back(stoi(two[0]));
+		y2.push_back(stoi(two[1]));
 	}
 
-	int max_x = std::max(
-		*std::max_element(x1.begin(), x1.end()),
-		*std::max_element(x2.begin(), x2.end())
+	int max_x = max(
+		*max_element(x1.begin(), x1.end()),
+		*max_element(x2.begin(), x2.end())
 	);
-	int max_y = std::max(
-		*std::max_element(y1.begin(), y1.end()),
-		*std::max_element(y2.begin(), y2.end())
+	int max_y = max(
+		*max_element(y1.begin(), y1.end()),
+		*max_element(y2.begin(), y2.end())
 	);
 
-	std::vector<std::vector<int>> vents;
+	vector<vector<int>> vents;
 
 	for (int y = 0; y <= max_y; y++)
 	{
-		vents.push_back(std::vector<int>());
+		vents.push_back(vector<int>());
 		for (int x = 0; x <= max_x; x++)
 			vents[y].push_back(0);
 	}
@@ -119,13 +119,13 @@ void Day5::part2()
 
 		if (x_one == x_two)
 		{
-			if (y_one > y_two) std::swap(y_one, y_two);
+			if (y_one > y_two) swap(y_one, y_two);
 			for (int y = y_one; y <= y_two; y++)
 				vents[y][x_one]++;
 		}
 		else if (y_one == y_two)
 		{
-			if (x_one > x_two) std::swap(x_one, x_two);
+			if (x_one > x_two) swap(x_one, x_two);
 			for (int x = x_one; x <= x_two; x++)
 				vents[y_one][x]++;
 		}
@@ -133,8 +133,8 @@ void Day5::part2()
 		{
 			if (x_one > x_two)
 			{
-				std::swap(x_one, x_two);
-				std::swap(y_one, y_two);
+				swap(x_one, x_two);
+				swap(y_one, y_two);
 			}
 
 			for (int o = 0; o <= x_two - x_one; o++)
@@ -148,5 +148,5 @@ void Day5::part2()
 			if (vents[i][cell] >= 2)
 				counter++;
 
-	std::cout << "Day:  5 | Part: 2 | Result: " << counter << std::endl;
+	cout << "Day:  5 | Part: 2 | Result: " << counter << endl;
 }
